738af26ae295ce8a5729380924f1dc006a5a34e4
YousraMaged <yousramaged8@gmail.com>
2020-03-28 14:14:35 +0200

    deploy

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files....
+// Serve static files
 app.use(express.static(__dirname + '/dist/news-app'));
 
 // Send all requests to index.html
